dbf63d87d917cea0c0976716920ee1e6b51e5efd
ZaccJacc <58274381+ZaccJacc@users.noreply.github.com>
2023-01-13 20:00:33 +0000

    hehe
    i think ive prolly done quite a bit since last time tbh

--- App.axaml.cs
+++ App.axaml.cs
@@ -4,23 +4,35 @@ using Avalonia.Markup.Xaml;
 using System;
 using System.Threading.Tasks;
 using SceneryStream.src;
+using System.Diagnostics;
 
 namespace SceneryStream
 {
     public partial class App : Application
     {
-        internal static LocalMachine ServiceInstance = new LocalMachine();
+        public static LocalMachine ServiceInstance = new LocalMachine();
 
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
         }
 
-        public override void OnFrameworkInitializationCompleted()
+        public override async void OnFrameworkInitializationCompleted()
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
+                Task platformbuild = ServiceInstance.BuildServiceAuthenticity();
                 desktop.MainWindow = new MainWindow();
+                //----//
+                await platformbuild;
+                if (!ServiceInstance.Platform_Verified)
+                {
+                    Console.WriteLine("[!] Unable to complete platform verification");
+                }
+                if (!ServiceInstance.Connected)
+                {
+                    Console.WriteLine("[!] Drive connection unsuccessful");
+                }
             }
 
             base.OnFrameworkInitializationCompleted();
--- src/LocalMachine.cs
+++ src/LocalMachine.cs
@@ -1,34 +1,67 @@
-﻿using System;
+﻿#pragma warning disable CS1998
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Net.NetworkInformation;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.IO;
 
 namespace SceneryStream.src
 {
-    internal class LocalMachine
+    public class LocalMachine
     {
         //vvvvvvvvvvvvvv//DEBUG VALUES//vvvvvvvvvvvvvv//
-        private const string ADDRESS = "192.168.1.143";
+        private const string ADDRESS = @"192.168.1.230\zachary";
         //^^^^^^^^^^^^^^//DEBUG VALUES//^^^^^^^^^^^^^^//
         private bool primary_connection_success;
+        public bool Connected { get { return primary_connection_success; } }
 
-        public Task<bool> platform_authenticity;
-        public bool Connected { get; }
+        private bool platform_authenticity;
+        public bool Platform_Verified { get { return platform_authenticity; } }
 
-        public LocalMachine()
+
+        /// <summary>
+        /// System used for platform verification and non-user-controlled functionality.
+        /// </summary>
+        public LocalMachine() { }
+
+        /// <summary>
+        /// Verify the operating system that .NET is running on so that the correct method for target server mounting can be used.<br/>
+        /// Verify that the target socket is responsive.<br/>
+        /// Attempt to mount the target server location specified in the application configuration settings (TBC)
+        /// </summary>
+        public async Task BuildServiceAuthenticity()
         {
-            platform_authenticity = BuildServiceAuthenticity();
-            Connected = primary_connection_success;
+            Console.WriteLine("[*] Verifying local platform");
+            Task<bool> verifyPlatform = VerifyLocalPlatform();
+            Task<bool> pingServer = AttemptAddressPing(ADDRESS);
+
+            platform_authenticity = await verifyPlatform;
+
+            bool ping_success = await pingServer;
+            if (!ping_success)
+            {
+                Console.WriteLine("[!] Initial server connection could not be made.\nVerify target socket in connection settings."); //Replace with viewable output in final production
+            } 
+            else
+            {
+                using var cts = new CancellationTokenSource();
+                cts.CancelAfter(2000); //change this for release
+                Task<bool> attempt_mounting = Utility.Windows.System.PerformTargetLocationMounting(ADDRESS, "X", cts.Token);
+                primary_connection_success = await attempt_mounting;
+            }
+            platform_authenticity = true;
         }
 
-        private async Task<bool> VerifyLocalPlatform()
+
+        private static async Task<bool> VerifyLocalPlatform() //this is throwing a warning - but don't be concerned, it means the contents ot this method will run sync. but that is fine.
         {
             PlatformID system = Environment.OSVersion.Platform;
-            await Task.Delay(4000); //DON'T LEAVE THIS WHEN YOU ACTUALLY NEED TO DO IT!!!!
-            Console.WriteLine(system);
+            Console.WriteLine("[*] System platform: " + system); //DEBUG
             switch (system)
             {
                 case PlatformID.Win32NT:
@@ -38,74 +71,192 @@ namespace SceneryStream.src
             }
         }
 
-        public async Task<bool> BuildServiceAuthenticity()
+        private static async Task<bool> AttemptAddressPing(string address)
         {
-            Console.WriteLine("[*] Verifying local platform");
-            Task<bool> verifyPlatform = VerifyLocalPlatform();
-            Task<bool> pingServer = AttemptAddressPing(ADDRESS);
-
-            bool platform_verification_success = await verifyPlatform;
-            if (!platform_verification_success){
-                throw new Exception("Could not verify platform");
-            }
-            bool ping_success = await pingServer;
-            if (!ping_success)
-            {
-                Console.WriteLine("[*] Initial server connection could not be made.\nVerify target socket in connection settings.");
-            } else
-            {
-                Task<bool> mount_location = PerformTargetLocationMounting(ADDRESS, "X", true);
-
-                bool mount_success = await mount_location;
-                if (mount_success)
+            string[] brokenaddress = address.Split("\\");
+            address = brokenaddress[0];
+                try
                 {
-                    primary_connection_success = true;
+                    PingReply reply = new Ping().Send(address, 1000);
+                    if (reply != null)
+                    {
+                        Console.WriteLine("[*] Ping Results \n\tStatus :  " + reply.Status + " \n\t Time : " + reply.RoundtripTime.ToString() + " \n\t Address : " + reply.Address); //DBUG
+                    }
+                    
+                    return true;
                 }
-            }
-            return true;
+                catch
+                {
+                    Console.WriteLine("[!] Untraced connection error."); //DBUG
+                    return false;
+                }
         }
 
-        private async Task<bool> PerformTargetLocationMounting(string address, string drive, bool debug_window)
+        
+
+        
+    }
+}
+
+
+
+namespace Utility
+{
+    namespace MacOS { }
+    namespace Linux { }
+    namespace Windows
+    {
+        internal class System
         {
-            try
+            internal static async Task<bool> PerformTargetLocationMounting(string address, string drive, CancellationToken ct)
             {
-                Process p = new Process();
+                Console.WriteLine("[*] Attempting target mounting");
+               
+                return await Task.Run(async () =>
+                {
+                    try
+                    {
+                        ct.ThrowIfCancellationRequested();
+                        await Task.Delay(5000).ConfigureAwait(false);
+                        NetworkDrive.MapNetworkDrive(drive, address);
+                        Console.WriteLine("[@] Debug point A1");
+                        return true;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("[!] Untraced mounting error.");
+                        return false;
+                    }
+                /*Process p = new Process();
+                await Task.Delay(13000).ConfigureAwait(false);
                 p.StartInfo.UseShellExecute = false;
                 p.StartInfo.RedirectStandardOutput = true;
-                p.StartInfo.FileName = "net";
-                p.StartInfo.Arguments = $"use {drive.ToUpper()}: \\\\{address}\\";
-                p.StartInfo.CreateNoWindow = !debug_window;
+                p.StartInfo.FileName = "cmd";
+                p.StartInfo.Arguments = $"/K net use {drive.ToUpper()}: \\\\{address}"; //Might need some better formatting surrounding this address...
+                p.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
                 p.Start();
                 p.WaitForExit();
-                //string output = p.StandardOutput.ReadToEnd();
+                string output = p.StandardOutput.ReadToEnd();
+                Console.WriteLine(output);
                 p.Dispose();
-                return true;
+                Console.WriteLine("[|] Target mounting complete");
+                return true;*/
+                });//.WaitAsync(TimeSpan.FromMilliseconds(12000)); //The drive mounting has 12 seconds to complete, or the task will timeout.
+
             }
-            catch
-            {
-                Console.WriteLine("[!] Untraced error");
-                return false;
-            }
-            
         }
 
-        private async Task<bool> AttemptAddressPing(string address)
+        public class NetworkDrive //Code sourced from StackOverflow (obviously) from users Mario and Mat
         {
-            try
+            private enum ResourceScope
             {
-                PingReply reply = new Ping().Send(address, 1000);
-                if (reply != null)
+                RESOURCE_CONNECTED = 1,
+                RESOURCE_GLOBALNET,
+                RESOURCE_REMEMBERED,
+                RESOURCE_RECENT,
+                RESOURCE_CONTEXT
+            }
+            private enum ResourceType
+            {
+                RESOURCETYPE_ANY,
+                RESOURCETYPE_DISK,
+                RESOURCETYPE_PRINT,
+                RESOURCETYPE_RESERVED
+            }
+            private enum ResourceUsage
+            {
+                RESOURCEUSAGE_CONNECTABLE = 0x00000001,
+                RESOURCEUSAGE_CONTAINER = 0x00000002,
+                RESOURCEUSAGE_NOLOCALDEVICE = 0x00000004,
+                RESOURCEUSAGE_SIBLING = 0x00000008,
+                RESOURCEUSAGE_ATTACHED = 0x00000010
+            }
+            private enum ResourceDisplayType
+            {
+                RESOURCEDISPLAYTYPE_GENERIC,
+                RESOURCEDISPLAYTYPE_DOMAIN,
+                RESOURCEDISPLAYTYPE_SERVER,
+                RESOURCEDISPLAYTYPE_SHARE,
+                RESOURCEDISPLAYTYPE_FILE,
+                RESOURCEDISPLAYTYPE_GROUP,
+                RESOURCEDISPLAYTYPE_NETWORK,
+                RESOURCEDISPLAYTYPE_ROOT,
+                RESOURCEDISPLAYTYPE_SHAREADMIN,
+                RESOURCEDISPLAYTYPE_DIRECTORY,
+                RESOURCEDISPLAYTYPE_TREE,
+                RESOURCEDISPLAYTYPE_NDSCONTAINER
+            }
+            [StructLayout(LayoutKind.Sequential)]
+            private struct NETRESOURCE
+            {
+                public ResourceScope oResourceScope;
+                public ResourceType oResourceType;
+                public ResourceDisplayType oDisplayType;
+                public ResourceUsage oResourceUsage;
+                public string sLocalName;
+                public string sRemoteName;
+                public string sComments;
+                public string sProvider;
+            }
+            [DllImport("mpr.dll")]
+            private static extern int WNetAddConnection2
+                (ref NETRESOURCE oNetworkResource, string sPassword,
+                string sUserName, int iFlags);
+
+            [DllImport("mpr.dll")]
+            private static extern int WNetCancelConnection2
+                (string sLocalName, uint iFlags, int iForce);
+
+            public static void MapNetworkDrive(string sDriveLetter, string sNetworkPath)
+            {
+                //Checks if the last character is \ as this causes error on mapping a drive.
+                if (sNetworkPath.Substring(sNetworkPath.Length - 1, 1) == @"\")
                 {
-                    Console.WriteLine("[*] Ping Results \n\tStatus :  " + reply.Status + " \n\t Time : " + reply.RoundtripTime.ToString() + " \n\t Address : " + reply.Address);
+                    sNetworkPath = sNetworkPath.Substring(0, sNetworkPath.Length - 1);
                 }
-                return true;
-            } 
-            catch
+
+                NETRESOURCE oNetworkResource = new NETRESOURCE()
+                {
+                    oResourceType = ResourceType.RESOURCETYPE_DISK,
+                    sLocalName = sDriveLetter + ":",
+                    sRemoteName = sNetworkPath
+                };
+
+                //If Drive is already mapped disconnect the current 
+                //mapping before adding the new mapping
+                if (IsDriveMapped(sDriveLetter))
+                {
+                    DisconnectNetworkDrive(sDriveLetter, true);
+                }
+
+                WNetAddConnection2(ref oNetworkResource, null, null, 0);
+            }
+
+            public static int DisconnectNetworkDrive(string sDriveLetter, bool bForceDisconnect)
             {
-                Console.WriteLine("[!] Untraced connection error.");
+                if (bForceDisconnect)
+                {
+                    return WNetCancelConnection2(sDriveLetter + ":", 0, 1);
+                }
+                else
+                {
+                    return WNetCancelConnection2(sDriveLetter + ":", 0, 0);
+                }
+            }
+
+            public static bool IsDriveMapped(string sDriveLetter)
+            {
+                string[] DriveList = Environment.GetLogicalDrives();
+                for (int i = 0; i < DriveList.Length; i++)
+                {
+                    if (sDriveLetter + ":\\" == DriveList[i].ToString())
+                    {
+                        return true;
+                    }
+                }
                 return false;
             }
-            
         }
     }
 }
+
